90b9ac158c37b4e6dfbb85c4a94fd529f2e03662
gessesantosarq-gsea <gessesantosarq@gmail.com>
2026-07-20 15:46:43 -0300

    Adicionar tour Marzipano

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-terrao",
-      "name": "Terraço",
+      "id": "0-hall",
+      "name": "Hall",
       "levels": [
         {
           "tileSize": 256,
@@ -26,17 +26,41 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.032390988989023484,
-          "pitch": 0.04969723591266373,
+          "yaw": 0.7865162925158291,
+          "pitch": 0.3578922177264534,
           "rotation": 0,
-          "target": "8-cozinha"
+          "target": "1-cozinha"
+        },
+        {
+          "yaw": 0.2662741399884858,
+          "pitch": 0.06554568880887324,
+          "rotation": 0,
+          "target": "4-terrao"
+        },
+        {
+          "yaw": -0.1293929703689667,
+          "pitch": 0.4480047946192478,
+          "rotation": 0,
+          "target": "2-jantar"
+        },
+        {
+          "yaw": 0.6702591648863567,
+          "pitch": 0.21181123938082536,
+          "rotation": 0,
+          "target": "3-estar"
+        },
+        {
+          "yaw": -0.38737282020597164,
+          "pitch": 0.5709092508380387,
+          "rotation": 0,
+          "target": "5-circulao"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-closet",
-      "name": "Closet",
+      "id": "1-cozinha",
+      "name": "Cozinha",
       "levels": [
         {
           "tileSize": 256,
@@ -60,16 +84,34 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.8460157504977452,
-          "pitch": 0.2869169954889088,
+          "yaw": -0.0235589844493731,
+          "pitch": 0.11434688775927704,
+          "rotation": 0,
+          "target": "4-terrao"
+        },
+        {
+          "yaw": 0.48522911758000653,
+          "pitch": 0.4104289444819855,
+          "rotation": 0,
+          "target": "3-estar"
+        },
+        {
+          "yaw": -0.8377155516088965,
+          "pitch": 0.3321659807696662,
+          "rotation": 0,
+          "target": "2-jantar"
+        },
+        {
+          "yaw": -1.2217977716706194,
+          "pitch": 0.19993640651177458,
           "rotation": 0,
           "target": "5-circulao"
         },
         {
-          "yaw": 2.146866077348399,
-          "pitch": 0.27267083548305493,
+          "yaw": -1.7006326210331988,
+          "pitch": 0.4910939667429215,
           "rotation": 0,
-          "target": "7-sute"
+          "target": "0-hall"
         }
       ],
       "infoHotspots": []
@@ -100,102 +142,138 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.7010213697796832,
-          "pitch": 0.22641774294528005,
+          "yaw": -0.5900978720464387,
+          "pitch": 0.1940047342973159,
           "rotation": 0,
-          "target": "8-cozinha"
+          "target": "3-estar"
         },
         {
-          "yaw": 1.4681497287304301,
-          "pitch": 0.3775175915174813,
+          "yaw": 0.7611502549541456,
+          "pitch": 0.2380338226986236,
           "rotation": 0,
-          "target": "3-hall"
+          "target": "1-cozinha"
         },
         {
-          "yaw": -0.5768173970131159,
-          "pitch": 0.23326784407164247,
+          "yaw": 1.4935174661412987,
+          "pitch": 0.31959875533199344,
           "rotation": 0,
-          "target": "6-estar"
+          "target": "0-hall"
         },
         {
-          "yaw": -1.0101140324587483,
-          "pitch": 0.16609179424315812,
-          "rotation": 0,
-          "target": "0-terrao"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-hall",
-      "name": "Hall",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 750,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.26712974879147744,
-          "pitch": 0.04415649818592904,
-          "rotation": 0,
-          "target": "0-terrao"
-        },
-        {
-          "yaw": -0.10732853702372225,
-          "pitch": 0.47148520373117186,
-          "rotation": 0,
-          "target": "2-jantar"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-lavabo",
-      "name": "Lavabo",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 750,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.2803825100341903,
-          "pitch": 0.6647516434245784,
+          "yaw": 1.8049177397106995,
+          "pitch": 0.9135130882385081,
           "rotation": 0,
           "target": "5-circulao"
+        },
+        {
+          "yaw": -1.0295886651502961,
+          "pitch": 0.1435285566844975,
+          "rotation": 0,
+          "target": "4-terrao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-estar",
+      "name": "Estar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8231674895228949,
+          "pitch": 0.06495487775015185,
+          "rotation": 0,
+          "target": "1-cozinha"
+        },
+        {
+          "yaw": 1.745730882751868,
+          "pitch": 0.1253024180982738,
+          "rotation": 0,
+          "target": "2-jantar"
+        },
+        {
+          "yaw": 1.2688059540947325,
+          "pitch": -0.013552386646550119,
+          "rotation": 0,
+          "target": "0-hall"
+        },
+        {
+          "yaw": 1.5934588553401365,
+          "pitch": 0.21358502028800608,
+          "rotation": 0,
+          "target": "5-circulao"
+        },
+        {
+          "yaw": -2.684575991723733,
+          "pitch": 0.5000794383371794,
+          "rotation": 0,
+          "target": "4-terrao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-terrao",
+      "name": "Terraço",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.38130997384091536,
+          "pitch": 0.28234117786383983,
+          "rotation": 0,
+          "target": "3-estar"
+        },
+        {
+          "yaw": -0.06254629081943364,
+          "pitch": 0.036638021853683256,
+          "rotation": 0,
+          "target": "1-cozinha"
+        },
+        {
+          "yaw": 0.26946357955747047,
+          "pitch": 0.09352185691987103,
+          "rotation": 0,
+          "target": "0-hall"
         }
       ],
       "infoHotspots": []
@@ -226,23 +304,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.3985675618214568,
-          "pitch": 0.37151031526194345,
+          "yaw": -0.1001035736691609,
+          "pitch": 0.32226621984949944,
           "rotation": 0,
-          "target": "4-lavabo"
+          "target": "6-closet"
         },
         {
-          "yaw": 0.008526516464151257,
-          "pitch": 0.36310893168341885,
+          "yaw": -0.38931811011029716,
+          "pitch": 0.47887607213593775,
           "rotation": 0,
-          "target": "1-closet"
+          "target": "9-lavabo"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-estar",
-      "name": "Estar",
+      "id": "6-closet",
+      "name": "Closet",
       "levels": [
         {
           "tileSize": 256,
@@ -266,32 +344,20 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.6691153308836633,
-          "pitch": 0.4735243509987761,
+          "yaw": 1.1236324588482258,
+          "pitch": 0.48832805696736337,
           "rotation": 0,
-          "target": "0-terrao"
+          "target": "8-wc-sute"
         },
         {
-          "yaw": 1.771967510296518,
-          "pitch": 0.15581849099582712,
+          "yaw": 2.180926327767713,
+          "pitch": 0.2450892456445768,
           "rotation": 0,
-          "target": "2-jantar"
+          "target": "7-quarto"
         },
         {
-          "yaw": 0.8861518013701364,
-          "pitch": 0.04672282044035292,
-          "rotation": 0,
-          "target": "8-cozinha"
-        },
-        {
-          "yaw": 1.2809381464740426,
-          "pitch": 0.021698534831790184,
-          "rotation": 0,
-          "target": "3-hall"
-        },
-        {
-          "yaw": 1.5994328403138258,
-          "pitch": 0.056347407242434144,
+          "yaw": 2.8533312906659027,
+          "pitch": 0.3204242442742178,
           "rotation": 0,
           "target": "5-circulao"
         }
@@ -299,8 +365,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "7-sute",
-      "name": "Suíte",
+      "id": "7-quarto",
+      "name": "Quarto",
       "levels": [
         {
           "tileSize": 256,
@@ -324,17 +390,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.8357580839927428,
-          "pitch": 0.142719564309866,
+          "yaw": 1.7855152800525262,
+          "pitch": 0.20320715592788474,
           "rotation": 0,
-          "target": "1-closet"
+          "target": "6-closet"
+        },
+        {
+          "yaw": 1.428716227860793,
+          "pitch": 0.2617875272782868,
+          "rotation": 0,
+          "target": "5-circulao"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "8-cozinha",
-      "name": "Cozinha",
+      "id": "8-wc-sute",
+      "name": "WC Suíte",
       "levels": [
         {
           "tileSize": 256,
@@ -358,38 +430,54 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.2315831152816372,
-          "pitch": 0.35424977657153,
+          "yaw": -3.1133353208054757,
+          "pitch": 0.37278760818685264,
+          "rotation": 0,
+          "target": "6-closet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-lavabo",
+      "name": "Lavabo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0031640244794833,
+          "pitch": 0.5708889938435142,
           "rotation": 0,
           "target": "5-circulao"
-        },
-        {
-          "yaw": -0.7359655667881508,
-          "pitch": 0.27104752274250465,
-          "rotation": 0,
-          "target": "2-jantar"
-        },
-        {
-          "yaw": 0.22791608529239937,
-          "pitch": 0.25703589440040275,
-          "rotation": 0,
-          "target": "6-estar"
-        },
-        {
-          "yaw": -0.0028187855704437936,
-          "pitch": 0.07769604001927988,
-          "rotation": 0,
-          "target": "0-terrao"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "AG7 - PACE - Mudra - AP Final 3",
+  "name": "AG7_PACE_MUDRA_AP FINAL 3",
   "settings": {
-    "mouseViewMode": "drag",
+    "mouseViewMode": "qtvr",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": false
+    "viewControlButtons": true
   }
 };
